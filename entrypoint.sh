a9b450d3d0a54ee6699d18f3d0f4c33181781bcb
senorgeneral <46357076+senorgeneral@users.noreply.github.com>
2019-01-04 14:09:33 -0500

    finds ngrok url and passes it to the plex python script

--- entrypoint.sh
+++ entrypoint.sh
@@ -80,5 +80,6 @@ ARGS="$ARGS `echo $NGROK_PORT | sed 's|^tcp://||'`"
 
 set -x
 exec $ARGS
+
 NGROK_URL = curl -s "$NGROK_PORT"/api/tunnels | grep -o 'https:\/\/[a-zA-Z0-9\.]\'
 python plexurl.py $NGROK_URL $PLEX_USER $PLEX_PWORD $PLEX_SERVER
