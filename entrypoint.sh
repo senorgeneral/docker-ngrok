7076bee71713982784954c601cee26a698531206
senorgeneral <46357076+senorgeneral@users.noreply.github.com>
2019-01-07 14:48:41 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -81,5 +81,5 @@ ARGS="$ARGS `echo $NGROK_PORT | sed 's|^tcp://||'`"
 set -x
 exec $ARGS
 
-NGROK_URL = curl -s "$NGROK_PORT"/api/tunnels | grep -o 'https:\/\/[a-zA-Z0-9\.]\'
+NGROK_URL = curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"https:..([^"]*).*/\1/p'
 python3 plexurl.py $NGROK_URL $PLEX_USER $PLEX_PWORD $PLEX_SERVER
