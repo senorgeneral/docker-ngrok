1d93c82296ff51bfd0925638bc05e2d7ba143e95
senorgeneral <46357076+senorgeneral@users.noreply.github.com>
2019-01-08 03:56:01 -0500

    Update plexurl.py

--- plexurl.py
+++ plexurl.py
@@ -12,9 +12,10 @@ def get_ngrok_url():
     res = requests.get(url)
     res_unicode = res.content.decode("utf-8")
     res_json = json.loads(res_unicode)
-    return res_json["tunnels"][1]["public_url"]
-
-# print(get_ngrok_url())
+    for i in res_json["tunnels"]:
+       if i['name'] == 'command_line':
+          return i['public_url']
+          break
 
 # Load user defined config
 NGROK_URL = get_ngrok_url() + ":443"
@@ -27,7 +28,6 @@ account = MyPlexAccount(PLEX_USER, PLEX_PWORD)
 plex = account.resource(PLEX_SERVER).connect()  # returns a PlexServer instance
 
 print(plex.settings.customConnections)
-#NGROK_URL = 'http://4743a8af.ngrok.io:80'
 customUrl = plex.settings.get('customConnections')
 customUrl.set(NGROK_URL)
 plex.settings.save()
