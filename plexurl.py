559b5922dbfd337ac62a367a2a356656bc98c205
senorgeneral <46357076+senorgeneral@users.noreply.github.com>
2019-01-07 22:06:39 -0500

    Update plexurl.py

--- plexurl.py
+++ plexurl.py
@@ -1,9 +1,21 @@
 #!/usr/bin/python3
 
 import sys
+import json
+import requests
+
+
+def get_ngrok_url():
+    url = "http://localhost:4040/api/tunnels"
+    res = requests.get(url)
+    res_unicode = res.content.decode("utf-8")
+    res_json = json.loads(res_unicode)
+    return res_json["tunnels"][0]["public_url"]
+
+# print(get_ngrok_url())
 
 # Load user defined config
-NGROK_URL = "https://" + sys.argv[1] + ":443"
+NGROK_URL = get_ngrok_url() + ":443"
 PLEX_USER = sys.argv[2]
 PLEX_PWORD = sys.argv[3]
 PLEX_SERVER = sys.argv[4]
