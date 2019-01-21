5426be32fc0b90a9f239d4e34703318abf0dce2c
senorgeneral <46357076+senorgeneral@users.noreply.github.com>
2019-01-21 13:28:43 -0500

    added comments

--- plexurl.py
+++ plexurl.py
@@ -4,9 +4,11 @@ import sys
 import json
 import requests
 import time
+
 # need time for ngrok to start up before requesting data
 time.sleep(15)
 
+# outputs the Ngrok https url
 def get_ngrok_url():
     url = "http://localhost:4040/api/tunnels"
     res = requests.get(url)
@@ -23,17 +25,21 @@ PLEX_USER = sys.argv[1]
 PLEX_PWORD = sys.argv[2]
 PLEX_SERVER = sys.argv[3]
 
+# stores plex user login info into a variable
 from plexapi.myplex import MyPlexAccount
 account = MyPlexAccount(PLEX_USER, PLEX_PWORD)
 plex = account.resource(PLEX_SERVER).connect()  # returns a PlexServer instance
 
+# displays current plex custom url settings. Not needed but nice to see
 print(plex.settings.customConnections)
+
+# sets new custom plex url with one from Ngrok  
 customUrl = plex.settings.get('customConnections')
 customUrl.set(NGROK_URL)
 plex.settings.save()
 
+# displays new custom plex url from Ngrok. Not needed but nice to see
 from plexapi.myplex import MyPlexAccount
 account = MyPlexAccount(PLEX_USER, PLEX_PWORD)
 plex = account.resource(PLEX_SERVER).connect()  # returns a PlexServer instance
-
 print(plex.settings.customConnections)
